d9fece21f3d2ff3097642a086b9260a1b7e14c95
ahmed yasser mohamed <89811009+ahmedyasser267@users.noreply.github.com>
2023-01-26 20:39:41 +0200

    Create A. Two Elevators.cpp

---
+++ A. Two Elevators.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+
+		int a, b, c;
+		cin >>  a >> b >> c;
+		long long a1 = abs(b - c) + abs(c - 1);
+		long long a2 = abs(a - 1);
+		if (a1 > a2)
+		{
+			cout << 1<<endl;
+
+		}
+		else if(a1<a2)
+		{
+			cout << 2<<endl;
+		}
+		else
+		{
+			cout << 3<<endl;
+		}
+
+	}
+	return 0;
+}
